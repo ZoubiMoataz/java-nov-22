3fc29977812ab429b511aba9a3c5b9ab1f6de7d9
ZoubiMoataz <141242489+ZoubiMoataz@users.noreply.github.com>
2023-08-13 19:47:56 +0300

    Create GithubFile.java

---
+++ src/GithubFile.java
@@ -0,0 +1,13 @@
+public class GithubFile {
+
+    public static void main(String[] args) {
+
+        System.out.println("      Hello Alex 1");
+        System.out.println();
+        System.out.print("                  Hello Alex 2");
+        System.out.println(" Hello Alex 3");
+        System.out.print("Hello Alex 4\n");
+        System.out.print("\tHello Alex 5");
+
+    }
+}
